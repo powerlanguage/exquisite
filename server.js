bb9e12f99a5cc5ad9abaa77b5c93874ebf39ac5d
Josh Wardle <josh.wardle@gmail.com>
2020-06-23 11:46:53 -0700

    Add shitty logging

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const server = require("http").createServer(app);
+const http = require("http");
 const path = require("path");
 
+const server = http.createServer(app);
+
 app.use(express.static(`${__dirname}/build`));
+
+// Shitty logger
+app.use((req, res, next) => {
+  console.log(req.path);
+  next();
+});
 
 app.get("/api/hello", (req, res) => {
   res.send(JSON.stringify({ express: "hello world" }));
