759d765ea13d0004aabce180286ff91d9e0c602e
Josh Wardle <josh.wardle@gmail.com>
2020-06-25 22:36:04 -0700

    Clean up info css, use alienblue

--- src/WhiteboardInfo.js
+++ src/WhiteboardInfo.js
@@ -1,46 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./WhiteboardInfo.module.css";
 
-const brushSizes = {
-  1: 3,
-  2: 6,
-  3: 12,
-  4: 18,
-};
-
-export default function WhiteboardInfo({
-  username,
-  handleChangeColor,
-  handleChangeBrushSize,
-  handleClear,
-  showControls,
-}) {
-  const [rawBrushSizeInput, setRawBrushInput] = useState(1);
-
-  const onChangeBrushSize = (rawValue) => {
-    setRawBrushInput(rawValue);
-    handleChangeBrushSize(brushSizes[rawValue]);
-  };
-
+export default function WhiteboardInfo({ username, isActive }) {
   return (
     <div className={styles.container}>
-      {showControls && (
-        <>
-          <input
-            type="color"
-            onChange={(e) => handleChangeColor(e.target.value)}
-          />
-          <input
-            type="range"
-            min="1"
-            max="4"
-            value={rawBrushSizeInput}
-            onChange={(e) => onChangeBrushSize(e.target.value)}
-          />
-          <button onClick={handleClear}>x</button>
-        </>
-      )}
-      <div className={styles.username}>{username}</div>
+      <div className={`${styles.username} ${isActive ? styles.active : ""}`}>
+        {username}
+      </div>
     </div>
   );
 }
